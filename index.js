63d956bc644fe1d18ad8e9a83fa6ca1280ea7f3e
Chely Ho <adept_inept@yahoo.com>
2017-11-06 23:52:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // Code your solutions in this file
 
 function printBadges(employeeNames){
-  for (let i=0; i <employeeNames.length; i++){
-    console.log('Welcome ${employeeNames[i]}! You are employee #' i+1);
-  }
+
   return employeeNames;
 }
+
