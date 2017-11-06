0a678b4fbe52d614eeec7e9b1c27df3b77921eaa
Chely Ho <adept_inept@yahoo.com>
2017-11-06 23:51:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 
-function printBadges (employeeNames) {
+function printBadges(employeeNames){
   for (let i=0; i <employeeNames.length; i++){
     console.log('Welcome ${employeeNames[i]}! You are employee #' i+1);
   }
